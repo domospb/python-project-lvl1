c19e727c2fe0430ded99975ea3ccdea972d3b608
domospb <d.artciukh@digitaldao.com>
2022-12-12 23:36:36 +0300

    Re-commit of 1st stage

---
+++ brain_games/scripts/brain_games.py
@@ -0,0 +1,10 @@
+
+def main():
+    print('Welcome to the Brain Games!')
+
+
+if __name__ == '__main__':
+    main()
+
+# poetry run python -m brain_games.scripts.brain_games
+# Welcome to the Brain Games!
