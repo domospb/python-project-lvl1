0ceefc9777b2e239b08efb480e8a3d87543da2e8
domospb <d.artciukh@digitaldao.com>
2022-12-13 08:21:54 +0300

    Stage 4, linter and CodeClimate

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -2,12 +2,10 @@
 
 from brain_games.cli import welcome_user
 
+
 def main():
     welcome_user()
 
 
 if __name__ == '__main__':
     main()
-
-# poetry run python -m brain_games.scripts.brain_games
-# Welcome to the Brain Games!
