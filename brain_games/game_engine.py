db6155e0c4cb1eee239a8c96e7af88ddce90625f
domospb <d.artciukh@digitaldao.com>
2023-07-21 12:53:08 +0300

    "while" replaced with "for/else" in game engine

--- brain_games/game_engine.py
+++ brain_games/game_engine.py
@@ -10,22 +10,19 @@ WELCOME_MESSAGE = "Welcome to the Brain Games!"
 def play(game):
     """Common game logic."""
     name: str = welcome_user()
-    score: int = 0
     print(game.DESCRIPTION)
-    while True:
+
+    for score in range(GAME_DURATION):
         correct_answer, expression = game.get_expression_and_answer()
         print(f"Question: {expression}")
         answer: str = prompt.string("Your answer: ")
-        if answer == correct_answer:
-            print("Correct!")
-            score += 1
-        else:
+
+        if answer != correct_answer:
             print(
                 f"""'{answer}' is wrong answer ;(. """
                 f"""Correct answer was '{correct_answer}'.\n"""
-                f"""Let's try again, {name}!"""
-            )
+                f"""Let's try again, {name}!""")
             break
-        if score == GAME_DURATION:
-            print(f"Congratulations, {name}!")
-            break
+        print("Correct!")
+    else:
+        print(f"Congratulations, {name}!")
